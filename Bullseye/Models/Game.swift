e9fd1053a56b2bc11ff2a11621e48cb49c61b0b5
Gospodi <gospodijazzet@gmail.com>
2021-12-09 12:57:34 +0300

    добавлена система начисление бонусных балов (bonus point) при 100% угадывании числа, приблизительном (с точностью 2 единицы - бонус 50 очков)

--- Bullseye/Models/Game.swift
+++ Bullseye/Models/Game.swift
@@ -8,13 +8,23 @@
 import Foundation
 
 struct Game {
-    var target: Int = Int.random(in: 1...100)
-    var score: Int = 0
-    var round: Int = 1
+    var target = Int.random(in: 1...100)
+    var score = 0
+    var round = 1
     
     
     func points(sliderValue: Int) -> Int {
-        return 100 - abs (target - sliderValue)
+        let difference = abs(target - sliderValue)
+        let bonus: Int
+        if difference == 0 {
+            bonus = 100
+        } else if difference <= 2 {
+            bonus = 50
+        } else {
+            bonus = 0
+        }
+        
+        return 100 - difference + bonus
     }
     
     mutating func startNewRound(points: Int) {
