cc9e7c08d9a714da13dc47cd3a5af004b333a372
Gospodi <gospodijazzet@gmail.com>
2021-12-10 11:59:34 +0300

    Приступил к верстке второго экрана с результатами в таблице рекордов LeaderboardView. Сделал отдельное представление добавил новый цветовой набор LeaderboardRowColor

---
+++ Bullseye/Views/LeaderBoardView.swift
@@ -0,0 +1,49 @@
+//
+//  LeaderBoardView.swift
+//  Bullseye
+//
+//  Created by Gospodi on 10.12.2021.
+//
+
+import SwiftUI
+
+struct LeaderBoardView: View {
+    var body: some View {
+        RowView(index: 1, score: 10, date: Date())
+    }
+}
+
+struct RowView: View {
+    let index: Int
+    let score: Int
+    let date: Date
+    
+    var body: some View {
+        HStack {
+        Slider(value: .constant(50.0))
+                .padding()
+        }
+        .background(
+            RoundedRectangle(
+                cornerRadius:
+                        .infinity)
+                .strokeBorder(Color("LeaderboardRowColor"),
+                              lineWidth: Constants.General.strokeWidth
+            )
+        )
+    }
+}
+
+
+struct LeaderBoardView_Previews: PreviewProvider {
+    static var previews: some View {
+        LeaderBoardView()
+        LeaderBoardView()
+            .previewLayout (.fixed(width: 568, height: 320))
+        LeaderBoardView()
+            .preferredColorScheme(.dark)
+        LeaderBoardView()
+            .preferredColorScheme(.dark)
+            .previewLayout (.fixed(width: 568, height: 320))
+    }
+}
